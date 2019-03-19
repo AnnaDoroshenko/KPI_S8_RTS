83ee417285d39bc7d694b539450c53574edee3ff
AnnaDoroshenko <annadoroshenko03@gmail.com>
2019-03-19 23:54:58 +0200

    added figure

--- Lab05/main.py
+++ Lab05/main.py
@@ -59,9 +59,9 @@ def erlang_by_mean(mean):
 class System:
     def __init__(self, lmbd, strategy):
         self.tasks = []
-        self.arrivals = [Arrival(0.1, TaskType.LARGE),\
+        self.arrivals = [Arrival(0.3, TaskType.LARGE),\
                 Arrival(0.2, TaskType.MEDIUM),\
-                Arrival(0.3, TaskType.SMALL)]
+                Arrival(0.1, TaskType.SMALL)]
         self.lmbd = lmbd
         self.strategy = strategy
     
@@ -75,8 +75,7 @@ class System:
     def add_task(self, new_task):
         # index for insertion
         index = len(self.tasks)
-        cmp_deadlines = True if (self.strategy == Strategy.EARLIEST_DEADLINE_FIRST)\
-                else False
+        cmp_deadlines = self.strategy == Strategy.EARLIEST_DEADLINE_FIRST
         while ((index > 0) and \
                 (new_task.less_important(self.tasks[index-1], cmp_deadlines))):
             index -= 1
@@ -92,9 +91,9 @@ class System:
             earliest_arrival_time = self.arrivals[-1].time
             earliest_finish_time = self.tasks[-1].left_time + current_time \
                     if (not len(self.tasks) == 0) else float('inf')
-            # decides which event happended first
+            # decides which event happened first
             if (earliest_arrival_time < earliest_finish_time):
-                # time jump to tne new event
+                # time jump to the new event
                 delta = earliest_arrival_time - current_time
                 current_time = earliest_arrival_time
                 # if there are no tasks in system => system was idle during time jump
@@ -117,7 +116,7 @@ class System:
                 if (current_time > finished_task.deadline): deadline_missed += 1
 
         # Statistics
-        system_load = 1 - (idle_time / current_time)
+        system_load = 1.0 - (idle_time / current_time)
 
         for task in self.tasks:
             if (task.deadline < current_time): deadline_missed += 1
@@ -133,33 +132,55 @@ if __name__ == "__main__":
     TICKS = 10000.0
     STRATEGY_RM = Strategy.RATE_MONOTONIC
     STRATEGY_EDF = Strategy.EARLIEST_DEADLINE_FIRST
-    stats_deadlines = []
-    stats_sys_load = []
-    stats_waiting_time = []
-    for l in np.arange(0.5, 0.9, 0.05):
-        system = System(lmbd = l, strategy = STRATEGY_EDF)
-        deadline_missed, system_load, waiting_time_mean = system.simulate(TICKS)
-        stats_deadlines.append(deadline_missed)
-        stats_sys_load.append(system_load)
-        stats_waiting_time.append(waiting_time_mean)
+    stats_deadlines_1, stats_deadlines_2  = [], []
+    stats_sys_load_1, stats_sys_load_2 = [], []
+    stats_waiting_time_1, stats_waiting_time_2 = [], []
+    for l in np.arange(0.05, 0.95, 0.05):
+        system_1 = System(lmbd = l, strategy = STRATEGY_RM)
+        deadline_missed_1, system_load_1, waiting_time_mean_1 = system_1.simulate(TICKS)
+        stats_deadlines_1.append(deadline_missed_1)
+        stats_sys_load_1.append(system_load_1)
+        stats_waiting_time_1.append(waiting_time_mean_1)
 
-    lambdas = np.arange(0.5, 0.9, 0.05)
+        system_2 = System(lmbd = l, strategy = STRATEGY_EDF)
+        deadline_missed_2, system_load_2, waiting_time_mean_2 = system_2.simulate(TICKS)
+        stats_deadlines_2.append(deadline_missed_2)
+        stats_sys_load_2.append(system_load_2)
+        stats_waiting_time_2.append(waiting_time_mean_2)
 
-    plt.subplot(3, 1, 1)
-    plt.plot(lambdas, stats_deadlines, 'k')
-    plt.ylabel('deadline_missed')
+    lambdas = np.arange(0.05, 0.95, 0.05)
+
+    plt.subplot(3, 2, 1)
+    plt.plot(lambdas, stats_deadlines_1, 'k')
+    plt.ylabel('rm_deadline_missed')
     plt.grid(True)
 
-    plt.subplot(3, 1, 2)
-    plt.plot(lambdas, stats_sys_load, 'b')
-    plt.ylabel('system_load')
+    plt.subplot(3, 2, 3)
+    plt.plot(lambdas, stats_sys_load_1, 'b')
+    plt.ylabel('rm_system_load')
     plt.grid(True)
 
-    plt.subplot(3, 1, 3)
-    plt.plot(lambdas, stats_waiting_time, 'c')
+    plt.subplot(3, 2, 5)
+    plt.plot(lambdas, stats_waiting_time_1, 'c')
     plt.xlabel('lambda')
-    plt.ylabel('waiting_time_mean')
+    plt.ylabel('rm_waiting_time_mean')
     plt.grid(True)
 
-    # plt.savefig('fig.png')
+    plt.subplot(3, 2, 2)
+    plt.plot(lambdas, stats_deadlines_2, 'k')
+    plt.ylabel('edf_deadline_missed')
+    plt.grid(True)
+
+    plt.subplot(3, 2, 4)
+    plt.plot(lambdas, stats_sys_load_2, 'b')
+    plt.ylabel('edf_system_load')
+    plt.grid(True)
+
+    plt.subplot(3, 2, 6)
+    plt.plot(lambdas, stats_waiting_time_2, 'c')
+    plt.xlabel('lambda')
+    plt.ylabel('edf_waiting_time_mean')
+    plt.grid(True)
+
+    plt.savefig('fig.png')
     plt.show()
